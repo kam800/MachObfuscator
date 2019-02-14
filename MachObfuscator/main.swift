d3585bee7dabb522edf153520d98fac5491bf9e1
Kamil Kosowski <kamil.kosowski@braintri.com>
2019-02-14 15:30:31 +0100

    Prints `Options.usage` when launch arguments resolving fails

--- MachObfuscator/main.swift
+++ MachObfuscator/main.swift
@@ -3,13 +3,13 @@ import Foundation
 private func main() {
     let options = Options.fromCommandLine()
     guard !options.help, let manglerType = options.manglerType, let appDirectory = options.appDirectory else {
+        print(Options.usage)
         return
     }
 
     LOGGER = SoutLogger(options: options)
     let mangler = manglerType.resolveMangler(machOViewDoomEnabled: options.machOViewDoom)
     let obfuscator = Obfuscator(directoryURL: appDirectory, mangler: mangler)
-    print(Options.usage)
     obfuscator.run()
 }
 
