befe5c6ac1ab0586e2ff87f20d12e6344ab3f496
Krzysztof Jamróz <krzysztof.jamroz@braintri.com>
2019-08-10 13:24:58 +0200

    #54 LC_BUILD_VERSION command unit tests

--- MachObfuscatorTests/MachTests/Samples/URL+SampleImages.swift
+++ MachObfuscatorTests/MachTests/Samples/URL+SampleImages.swift
@@ -10,4 +10,14 @@ extension URL {
     static var machoMacExecutable: URL {
         return Bundle(for: MarkerClass.self).url(forResource: "SampleMachoMacExecutable", withExtension: nil)!
     }
+
+    // To obtain this executable compile SampleMacApp with MACOSX_DEPLOYMENT_TARGET = 10.14
+    static var machoMac10_14Executable: URL {
+        return Bundle(for: MarkerClass.self).url(forResource: "SampleMachoMac10_14Executable", withExtension: nil)!
+    }
+
+    // To obtain this executable compile SampleIosApp with IPHONEOS_DEPLOYMENT_TARGET = 12.0
+    static var machoIos12_0Executable: URL {
+        return Bundle(for: MarkerClass.self).url(forResource: "SampleMachoIos12_0Executable", withExtension: nil)!
+    }
 }
