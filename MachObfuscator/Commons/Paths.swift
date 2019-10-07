26e872da6104367405a4ccb0b2711fcca71eb21a
Kamil Borzym <kamil.borzym@gmail.com>
2019-10-07 22:52:51 +0200

    [#53] Update Xcode iOS runtime path

--- MachObfuscator/Commons/Paths.swift
+++ MachObfuscator/Commons/Paths.swift
@@ -1,6 +1,23 @@
+import Foundation
+
 enum Paths {
     static let separator: Character = "/"
-    static let iosRuntimeRoot = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot"
     static let macosFrameworksRoot = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
     static let iosFrameworksRoot = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
+
+    static let iosRuntimeRoot: String = {
+        guard let iosRoot = possibleIosRuntimeRoots.first(where: FileManager.default.fileExists(atPath:)) else {
+            fatalError("Could not find iOS runtime root. Make sure you have Xcode (10/11) installed.")
+        }
+        return iosRoot
+    }()
+
+    private static let possibleIosRuntimeRoots: [String] = [
+        // Xcode 10
+        "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/"
+            + "Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot",
+        // Xcode 11
+        "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/"
+            + "Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot",
+    ]
 }
