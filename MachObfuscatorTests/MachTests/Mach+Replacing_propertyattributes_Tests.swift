500420093c372bd9f2ee99ab67109f0e4135ab49
Krzysztof Jamróz <krzysztof.jamroz@braintri.com>
2019-10-16 13:06:35 +0200

    Simplified obfuscation tests

--- MachObfuscatorTests/MachTests/Mach+Replacing_propertyattributes_Tests.swift
+++ MachObfuscatorTests/MachTests/Mach+Replacing_propertyattributes_Tests.swift
@@ -18,20 +18,17 @@ class Mach_Replacing_propertyattributes_Tests: XCTestCase {
         var sut = try! Image.load(url: URL.machoIos12_0Executable)
 
         // Prepare test obfuscation configuration satisfying requirements of replaceSymbols
-        let emptyTrie = Trie(exportsSymbol: false, labelRange: 0 ..< 0, label: [], children: [])
-        let firstImportEntry = sut.machs[0].importStack[0]
-        let symbolDylib = sut.machs[0].dylibs[firstImportEntry.dylibOrdinal - 1]
-
+        //
         // In normal case NSString would not be obfuscated. In test we use it because it is referenced in property types
         let map = SymbolManglingMap(selectors: [:], classNames: ["NSString": "ObfStrng"], exportTrieObfuscationMap: [
             URL.machoIos12_0Executable: [
                 sut.machs[0].cpu.asCpuId:
-                    (unobfuscated: emptyTrie,
-                     obfuscated: emptyTrie),
+                    (unobfuscated: Trie.empty,
+                     obfuscated: Trie.empty),
             ],
         ])
         var paths = ObfuscationPaths()
-        paths.resolvedDylibMapPerImageURL = [URL.machoIos12_0Executable: [symbolDylib: URL(fileURLWithPath: "/tmp/testlib")]]
+        paths.resolvedDylibMapPerImageURL = [URL.machoIos12_0Executable: [:]]
 
         // When
         sut.replaceSymbols(withMap: map, paths: paths)
